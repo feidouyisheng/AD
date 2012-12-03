4bdd3cd60e6aef1e2be38bdaed85670f50c99fdf
斌 <bear@bin-Mac.local>
2012-12-03 19:56:51 +0800

    Initial Commit

---
+++ AD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AD
+//
+//  Created by 斌 on 12-12-3.
+//  Copyright (c) 2012年 斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AD/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AD
+//
+//  Created by 斌 on 12-12-3.
+//  Copyright (c) 2012年 斌. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
